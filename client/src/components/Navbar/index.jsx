db0fb0fbf929d5b21862dae3d81ab2f4a0301fd2
Muhammad Iqbal <hellofromiqbal@gmail.com>
2024-02-28 10:54:30 +0700

    add cart icons

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -7,7 +7,12 @@ const Navbar = () => {
     <nav className='flex justify-between items-center h-14 px-8 border-b-[1px] shadow-sm'>
       <h1 className='text-2xl font-medium text-green-600'>MyStore</h1>
       <div className='flex items-center gap-4'>
-        <BsBag size={21}/>
+        <div className='flex relative'>
+          <div className='absolute -top-2 -right-2 rounded-full bg-red-500 w-5 h-5 flex justify-center items-center'>
+            <small className='text-white text-xs'>1</small>
+          </div>
+          <BsBag size={21}/>
+        </div>
         <ul className='flex gap-3 ps-4 border-s'>
           <li>
             <Button
