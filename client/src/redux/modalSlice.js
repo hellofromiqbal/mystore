f1dbb4979cbb632ac16f5549b6232dbc8f42621d
Muhammad Iqbal <hellofromiqbal@gmail.com>
2024-02-28 15:05:41 +0700

    add modalReducer

---
+++ client/src/redux/modalSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const modalSlice = createSlice({
+  name: 'modal',
+  initialState: {
+    modalType: '',
+    showModal: false
+  },
+  reducers: {
+    toggleModal: (state, action) => {
+      state.showModal = !state.showModal
+      if(state.showModal === false) {
+        state.modalType = ''
+      } else {
+        state.modalType = action.payload
+      }
+    },
+  }
+})
+
+export const { toggleModal } = modalSlice.actions;
+export const selectModal = (state) => state;
+export default modalSlice.reducer;
