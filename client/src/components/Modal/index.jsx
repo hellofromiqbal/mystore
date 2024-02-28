6a7e8423471e85a514f2c41f52f991ff91bc549a
Muhammad Iqbal <hellofromiqbal@gmail.com>
2024-02-28 14:51:10 +0700

    add react-redux, @reduxjs/toolkit and adjust Modal component

--- client/src/components/Modal/index.jsx
+++ client/src/components/Modal/index.jsx
@@ -4,7 +4,7 @@ import LoginForm from '../Forms/LoginForm'
 
 const Modal = () => {
   return (
-    <div className='fixed top-0 right-0 bottom-0 left-0 flex justify-center items-center bg-black bg-opacity-50'>
+    <div className='fixed z-10 top-0 right-0 bottom-0 left-0 flex justify-center items-center bg-black bg-opacity-50'>
       <div className='w-1/3 px-5 pt-4 pb-6 bg-white rounded-md flex flex-col'>
         <RegisterForm/>
         {/* <LoginForm/> */}
