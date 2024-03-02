caae9f7667dfd62bbe15b7c3aabfe9613978a525
Muhammad Iqbal <hellofromiqbal@gmail.com>
2024-03-02 21:52:23 +0700

    adjust cart ui

--- client/src/components/Cart/index.jsx
+++ client/src/components/Cart/index.jsx
@@ -3,9 +3,11 @@ import { useSelector, useDispatch } from 'react-redux';
 import { toggleModal } from '../../redux/modalSlice';
 import { IoCloseCircleOutline, IoCloseCircle } from "react-icons/io5";
 import { selectCurrUser } from '../../redux/currUserSlice';
+import { currencyFormatter } from '../../../helpers/currencyFormatter';
 
 const Cart = () => {
   const currUser = useSelector(selectCurrUser);
+  console.log(currUser);
   const dispatch = useDispatch();
   return (
     <div className='flex flex-col gap-2 relative'>
@@ -16,9 +18,20 @@ const Cart = () => {
         <IoCloseCircleOutline size={25}/>
       </button>
       <h2 className='text-2xl font-bold text-center'>Cart</h2>
-      <div>
-        <h1>This is your cart.</h1>
-      </div>
+      <ul className='flex flex-col border-t'>
+        {currUser?.cart?.map((cartItem) => (
+          <li key={cartItem._id} className='flex justify-between border-b py-2'>
+            <div>
+              <h3 className='font-medium text-lg'>{cartItem?.product?.name}</h3>
+              <p>{cartItem?.product?.description}</p>
+              <p className='font-medium'>{cartItem?.amount}</p>
+            </div>
+            <div className='self-end'>
+              <p className='font-medium'>{currencyFormatter.format(cartItem?.product?.price)}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 };
