0363b13f7b499fcf19cb57bd2294fd8e8a0e51da
Muhammad Iqbal <hellofromiqbal@gmail.com>
2024-03-10 05:53:27 +0700

    add admin dashboard page

---
+++ client/src/pages/Main/AdminDashboard/index.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { FaReceipt } from 'react-icons/fa';
+
+const AdminDashboard = () => {
+  return (
+    <section className='flex flex-wrap h-screen px-16 pt-24 pb-10'>
+      <div className='flex justify-center items-center basis-1/4'>
+        <div className='flex flex-col justify-center items-center border rounded-md w-[90%] h-[90%]'>
+          <FaReceipt size={80}/>
+          <h2>Invoices</h2>
+        </div>
+      </div>
+      <div className='flex justify-center items-center basis-1/4'>
+        <div className='flex flex-col justify-center items-center border rounded-md w-[90%] h-[90%]'>
+          <FaReceipt size={80}/>
+          <h2>Invoices</h2>
+        </div>
+      </div>
+      <div className='flex justify-center items-center basis-1/4'>
+        <div className='flex flex-col justify-center items-center border rounded-md w-[90%] h-[90%]'>
+          <FaReceipt size={80}/>
+          <h2>Invoices</h2>
+        </div>
+      </div>
+      <div className='flex justify-center items-center basis-1/4'>
+        <div className='flex flex-col justify-center items-center border rounded-md w-[90%] h-[90%]'>
+          <FaReceipt size={80}/>
+          <h2>Invoices</h2>
+        </div>
+      </div>
+      <div className='flex justify-center items-center basis-1/4'>
+        <div className='flex flex-col justify-center items-center border rounded-md w-[90%] h-[90%]'>
+          <FaReceipt size={80}/>
+          <h2>Invoices</h2>
+        </div>
+      </div>
+      <div className='flex justify-center items-center basis-1/4'>
+        <div className='flex flex-col justify-center items-center border rounded-md w-[90%] h-[90%]'>
+          <FaReceipt size={80}/>
+          <h2>Invoices</h2>
+        </div>
+      </div>
+      <div className='flex justify-center items-center basis-1/4'>
+        <div className='flex flex-col justify-center items-center border rounded-md w-[90%] h-[90%]'>
+          <FaReceipt size={80}/>
+          <h2>Invoices</h2>
+        </div>
+      </div>
+      <div className='flex justify-center items-center basis-1/4'>
+        <div className='flex flex-col justify-center items-center border rounded-md w-[90%] h-[90%]'>
+          <FaReceipt size={80}/>
+          <h2>Invoices</h2>
+        </div>
+      </div>
+    </section>
+  )
+};
+
+export default AdminDashboard;
--- client/src/pages/Main/index.jsx
+++ client/src/pages/Main/index.jsx
@@ -3,16 +3,22 @@ import AllMenuSection from './AllMenuSection/AllMenuSection';
 import JumboSection from './JumboSection/JumboSection';
 import { useSelector } from 'react-redux';
 import { selectCurrUser } from '../../redux/currUserSlice';
+import AdminDashboard from './AdminDashboard';
 
 const MainPage = () => {
   const currUser = useSelector(selectCurrUser);
   return (
     <div>
       {currUser?.role === 'user' || currUser?.role === undefined ?
-        <JumboSection/>
-        : ''
+        <>
+          <JumboSection/>
+          <AllMenuSection/>
+        </>
+        :
+        <div className='-mt-14'>
+          <AdminDashboard/>
+        </div>
       }
-      <AllMenuSection/>
     </div>
   )
 };
